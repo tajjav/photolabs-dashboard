880dacde3b3d664e4a64edc6e2df18346f0bb395
tajjav <taj.jav@hotmail.com>
2024-02-08 00:19:06 -0700

    add dashboard.js loading.js panel.js and rendering photolabs api data

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,113 @@
 import React, { Component } from "react";
-
+import Loading from "./Loading";
 import classnames from "classnames";
+import Panel from "./Panel";
+
+import {
+  getTotalPhotos,
+  getTotalTopics,
+  getUserWithMostUploads,
+  getUserWithLeastUploads
+ } from "helpers/selectors";
+
+const data = [
+  {
+    id: 1,
+    label: "Total Photos",
+    value: getTotalPhotos,
+  },
+  {
+    id: 2,
+    label: "Total Topics",
+    value: getTotalTopics,
+  },
+  {
+    id: 3,
+    label: "User with the most uploads",
+    value: getUserWithMostUploads,
+  },
+  {
+    id: 4,
+    label: "User with the least uploads",
+    value: getUserWithLeastUploads,
+  },
+];
 
 class Dashboard extends Component {
-  render() {
-    const dashboardClasses = classnames("dashboard");
+  state = {
+    loading: true,
+    focused: null,
+    photos: [],
+    topics: []
+  };
 
-    return <main className={dashboardClasses} />;
+  componentDidMount() {
+    const focused = JSON.parse(localStorage.getItem("focused"));
+
+    if (focused) {
+      this.setState({ focused });
+    }
+
+    const urlsPromise = [
+      "/api/photos",
+      "/api/topics",
+    ].map(url => fetch(url).then(response => response.json()));
+
+    Promise.all(urlsPromise)
+      .then(([photos, topics]) => {
+        this.setState({
+          loading: false,
+          photos: photos,
+          topics: topics
+        });
+      });
+
+  }
+
+  componentDidUpdate(previousProps, previousState) {
+    if (previousState.focused !== this.state.focused) {
+      localStorage.setItem("focused", JSON.stringify(this.state.focused));
+    }
+  }
+
+  // instance method
+  selectPanel(id) {
+    // console.log("this: ", this);
+    this.setState((previousState) => {
+      return {
+        focused: previousState.focused !== null 
+        ? null 
+        : id,
+      }
+    });
+  }
+
+  render() {
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused,
+    });
+
+    if (this.state.loading) {
+      return <Loading />;
+    }
+
+    const panels = (
+      this.state.focused
+        ? data.filter((panel) => this.state.focused === panel.id)
+        : data
+    ).map((panel) => (
+      <Panel
+        key={panel.id}
+        id={panel.id}
+        label={panel.label}
+        value={panel.value(this.state)}
+        onSelect={() => this.selectPanel(panel.id)}
+      />
+    ));
+    // console.log('data: ', data);
+    // console.log("panels: ", panels);
+
+    return <main className={dashboardClasses}>{panels}</main>;
   }
 }
 
---
+++ src/components/Loading.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+// import classnames from "classnames";
+
+class Loading extends Component {
+  // state = {loading : true}
+
+
+
+
+  render() {
+    
+    return <section className="loading" >Loading</section>;
+  }
+}
+
+export default Loading;
